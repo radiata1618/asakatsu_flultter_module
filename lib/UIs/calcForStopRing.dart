932831dbb1599e2e37c45d727217fc15c484e070
haruk <haruki.u@gmail.com>
2022-07-24 12:27:33 +0900

    画面修正中

---
+++ lib/UIs/calcForStopRing.dart
@@ -0,0 +1,54 @@
+import 'package:asakatsu_flultter_module/common/UI/commonTextUI.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:vibration/vibration.dart';
+import 'dart:math' as math;
+
+import '../common/UI/commonTextFormUI.dart';
+
+class CalcForStopRing extends ConsumerWidget {
+  const CalcForStopRing(
+    AudioPlayer audioPlayer, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    var rand = math.Random();
+    int a = rand.nextInt(100);
+    int b = rand.nextInt(100);
+    String calcStr = a.toString() + "+" + b.toString();
+    int answer = a + b;
+
+    final player = AudioPlayer();
+    player.play(AssetSource('sounds/bgm.wav'), volume: 100);
+    vibe();
+
+    return Scaffold(
+        body: Column(
+      children: [
+        Expanded(
+            child: Container(
+                color: Colors.black12, child: commonText16BlackLeft(calcStr))),
+        commonTextBoxBordered(text: 'answer',
+            onChanged: (String value) {
+                if(int.parse(value)==answer){
+                  stopSoundsAndVibe(player);
+                }
+            }),
+      ],
+    ));
+  }
+}
+
+Future<void> vibe() async {
+  Vibration.vibrate(duration: 300000);
+}
+
+void stopSoundsAndVibe(AudioPlayer player) {
+  player.pause();
+  player.dispose();
+  Vibration.cancel();
+}
--- lib/UIs/rootUI.dart
+++ lib/UIs/rootUI.dart
@@ -27,43 +27,31 @@ class Root extends ConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
 
     ref.read(bottomNavigationBarItemsProvider.notifier).initialize(ref);
-    final _selectedIndex = ref.watch(_selectedIndexProvider);
+    final selectedIndex = ref.watch(_selectedIndexProvider);
     checkRingData(ref,context);
 
     return Scaffold(
       body: routeElement(
-          _selectedIndex,
+          selectedIndex,
           ref),
+
       bottomNavigationBar: BottomNavigationBar(
           selectedItemColor:commonColorSecondary,
-          type: BottomNavigationBarType.fixed, // これを書かないと3つまでしか表示されない
+          type: BottomNavigationBarType.fixed,
           items: ref.watch(bottomNavigationBarItemsProvider.notifier)
               .bottomNavigationBarItems,
-          currentIndex: _selectedIndex,
-          //selectedItemColor: Colors.black,
+          currentIndex: selectedIndex,
           onTap: (int index) {
             ref.read(bottomNavigationBarItemsProvider.notifier)
-                .activateButton(_selectedIndex, index);
+                .activateButton(selectedIndex, index);
             ref.read(_selectedIndexProvider.state).update((state) => index);
-          }),
+          })
     );
   }
 
   Widget routeElement(
       int selectedIndex, WidgetRef ref) {
 
-    // if(ref.watch(userDataProvider).userData["userType"]=="1"){
-    //   switch (selectedIndex) {
-    //     case 0:
-    //       return SearchUsersMainPage();
-    //     case 1:
-    //       return Talk();
-    //     case 2:
-    //       return LessonListRoot();
-    //     default:
-    //       return MyPage();
-    //   }
-    // }else{
       switch (selectedIndex) {
         case 0:
           return const ViewAlarmTimePattern();
